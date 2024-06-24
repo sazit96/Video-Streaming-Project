20e91e841ae84a89cec8cce60426c6a44e6dada3
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2024-06-24 23:10:47 +0600

    today added rate limit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const port = 3000;
@@ -8,15 +9,28 @@ const port = 3000;
 // Enable CORS for all routes
 app.use(cors());
 
+// Rate limit for serving video files
+const videoRateLimitSegment = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 500,
+  message: 'Too many requests, Please try again leter.',
+});
+
+const videoRateLimitMpd = rateLimit({
+  windowMs: 1 * 60 * 1000, // 10 minutes
+  max: 6, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+});
+
 // Serve the DASH manifest file
-app.get('/video.mpd', (req, res) => {
+app.get('/video.mpd', videoRateLimitMpd, (req, res) => {
   console.log('called for mpd');
   const filePath = path.join(__dirname, 'video', 'video.mpd');
   res.sendFile(filePath);
 });
 
 // Serve the DASH segment files
-app.get('/video/:segment', (req, res) => {
+app.get('/video/:segment', videoRateLimitSegment, (req, res) => {
   console.log('called segment');
   const segment = req.params.segment;
   const filePath = path.join(__dirname, 'video', segment);
